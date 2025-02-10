cb3718ac1590c4b71e413989b919338249669cf6
John Vincent De Leon <johnvincent.deleon2004@gmail.com>
2025-02-10 22:36:25 +0800

    Created a main menu that keeps on displaying until the user wants to exit

--- Projects/Task_Manager.py
+++ Projects/Task_Manager.py
@@ -1,4 +1,24 @@
 def main():
-    print(" [1] Add Task \n [2] View Task \n [3] Mark Task as Complete \n [4] Save Task to a File \n [5] Load Task to a File")
+    print("TASK MANAGER")
+    print(" [1] Add Task \n [2] View Task \n [3] Mark Task as Complete \n [4] Save Task to a File \n [5] Load Task to a File \n [6] Exit")
+    choice = input("Please select an option: ")
+    return int(choice)
 
-main()
+running = True
+while running:
+    user_choice = main()
+    
+    if user_choice == 1:
+        pass
+    elif user_choice == 2:
+        pass
+    elif user_choice == 3:
+        pass
+    elif user_choice == 4:
+        pass
+    elif user_choice == 5:
+        pass
+    elif user_choice == 6:
+        running = False
+    else:
+        print("Please select the number that is only provided!")
